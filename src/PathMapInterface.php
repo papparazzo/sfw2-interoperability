471aa7857ea661e8d28c43b65240411b14bddf6b
papparazzo <pappi-@gmx.de>
2025-05-23 16:57:34 +0200

    PathMapInterface.php added

---
+++ src/PathMapInterface.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ *  SFW2 - SimpleFrameWork
+ *
+ *  Copyright (C) 2024  Stefan Paproth
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU Affero General Public License as
+ *  published by the Free Software Foundation, either version 3 of the
+ *  License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU Affero General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Affero General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/agpl.txt>.
+ *
+ */
+
+declare(strict_types=1);
+
+namespace SFW2\Interoperability;
+
+interface PathMapInterface
+{
+    public function hasPath(string $path, string $method = 'GET'): bool;
+
+    public function getPathId(string $path, string $method = 'GET'): int;
+
+    public function getPathById(int $pathId): string;
+}
